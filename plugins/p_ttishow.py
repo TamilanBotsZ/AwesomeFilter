10319b8eda1325dcdc07d8b8565674172caf1d57
haashim0 <103762873+haashim0@users.noreply.github.com>
2023-01-01 18:07:20 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -22,7 +22,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton(''✨ Uᴘᴅᴀᴛᴇ 👩‍💻, url=f'https://t.me/{SUPPORT_CHAT}')
+                InlineKeyboardButton('✨ Uᴘᴅᴀᴛᴇ 👩‍💻', url=f'https://t.me/{SUPPORT_CHAT}')
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(
